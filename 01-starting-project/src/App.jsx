f7a61e0c3aa7bf5a2617c57208b23d0e0c6945b8
jacegonzales25 <jacegonzales25@gmail.com>
2024-04-27 21:25:42 +0800

    feat: add stateful conditions

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("component");
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
+    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log("selectedTopic:", selectedButton);
+    console.log("EXAMPLES[selectedTopic]:", EXAMPLES[selectedButton]);
   }
 
   return (
@@ -31,14 +36,22 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("component")}>
+            <TabButton onSelect={() => handleSelect("components")}>
               Component
             </TabButton>
             <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}{" "}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
