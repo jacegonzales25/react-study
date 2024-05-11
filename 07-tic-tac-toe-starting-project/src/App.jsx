32b9efcc5a68798cfd0d4503f1d58b28928088b8
jacegonzales25 <jacegonzales25@gmail.com>
2024-05-11 20:53:28 +0800

    feat: change state to computed values and derive

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,13 +3,26 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
-
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X')
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [{ square: {row: rowIndex, col: colIndex}, player: currentPlayer } , ...prevTurns];
+      
+      return updatedTurns;
+    });
+
   }
 
 
@@ -20,8 +33,9 @@ function App() {
           <Player name={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'}/>
           <Player name={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
+      <Log turns={gameTurns}/>
     </main>
   );
 }
