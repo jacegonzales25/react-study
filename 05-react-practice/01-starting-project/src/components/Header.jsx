e367625d4f1fb2e8afbd01ae4d16316ad379c5f5
jacegonzales25 <jacegonzales25@gmail.com>
2024-05-19 12:12:39 +0800

    fix: small id fix to header

--- 05-react-practice/01-starting-project/src/components/Header.jsx
+++ 05-react-practice/01-starting-project/src/components/Header.jsx
@@ -2,9 +2,9 @@ import investmentImage from "../assets/investment-calculator-logo.png";
 
 export default function Header() {
   return (
-    <div id="header">
+    <header id="header">
       <img src={investmentImage} alt="Photo of a Money bag"/>
       <h1>Invesment Calculator</h1>
-    </div>
+    </header>
   );
 }
