b7629e19352f7027ddce827dc4877ed65219238a
jacegonzales25 <jacegonzales25@gmail.com>
2024-05-28 22:02:03 +0800

    feat: add useState to dynamically show NewProject component or NoProjectSelected fallback

--- 09-project-management/01-starting-project/src/App.jsx
+++ 09-project-management/01-starting-project/src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
