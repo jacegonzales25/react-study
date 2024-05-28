884a896e2be686bb05b6402350863603efd50901
jacegonzales25 <jacegonzales25@gmail.com>
2024-05-28 13:04:02 +0800

    feat: add sidebar

---
+++ 09-project-management/01-starting-project/src/components/NewProject.jsx
@@ -0,0 +1,14 @@
+export default function NewProject() {
+    return (
+        <div>
+            <menu>
+                <li>
+                    <button>Cancel</button>
+                </li>
+                <li>
+                    <button>Save</button>
+                </li>
+            </menu>
+        </div>
+    )
+}
