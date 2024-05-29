30b4fd2af52739ed2b00723457b50aab44d9c9e5
jacegonzales25 <jacegonzales25@gmail.com>
2024-05-29 22:54:20 +0800

    fix: minor fixes on save and git rebase

--- 09-project-management/01-starting-project/src/components/Modal.jsx
+++ 09-project-management/01-starting-project/src/components/Modal.jsx
@@ -14,10 +14,14 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   });
 
   return createPortal(
-    <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+    >
       {children}
       <form method="dialog" className="mt-4 text-right">
-        <Button>{buttonCaption}</Button>      </form>
+        <Button>{buttonCaption}</Button>{" "}
+      </form>
     </dialog>,
     document.getElementById("modal-root")
   );
