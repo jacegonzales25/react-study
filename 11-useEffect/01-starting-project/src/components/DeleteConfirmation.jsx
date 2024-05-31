2ec188a144a06400cd861d666102aa7a03deca05
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-01 00:41:26 +0800

    feat: finish useEffect lecture, bug: progress bar reset

--- 11-useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ 11-useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
 
-const TIMER = 3000
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-  useEffect(() => {
-   const interval =  setInterval(() => {
-setRemainingTime(prevTime => prevTime - 10);
-    }, 10)
-
-    return () => {
-      clearInterval(interval)
-    }
-    
-  }, [])
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -24,9 +12,9 @@ setRemainingTime(prevTime => prevTime - 10);
 
     return () => {
       clearTimeout(timer);
-    }
+    };
   }, [onConfirm]);
-  
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -39,7 +27,7 @@ setRemainingTime(prevTime => prevTime - 10);
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER}/>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
