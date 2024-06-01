5ae8f53284aeb2179c647c1bc34d380c645cc5ac
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-01 15:42:38 +0800

    feat: add timer progress bar to Quiz component

--- 12-quiz-app/01-starting-project/src/components/Quiz.jsx
+++ 12-quiz-app/01-starting-project/src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
 import quizComplete from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -14,7 +15,6 @@ export default function Quiz() {
     });
   }
 
- 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   if (quizIsComplete) {
@@ -26,14 +26,14 @@ export default function Quiz() {
     );
   }
 
-//   Added to the end for the return to pass as it goes top-down
+  //   Added to the end for the return to pass as it goes top-down
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   shuffledAnswers.sort(() => Math.random() - 0.5);
-
 
   return (
     <div id="quiz">
       <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={() => handleAnswerQuiz(null)}/>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
