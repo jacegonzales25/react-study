5aef62c92726f8f9df3d1c13151d0bc64287f310
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-01 15:09:21 +0800

    feat: add quiz section

--- 12-quiz-app/01-starting-project/src/components/Quiz.jsx
+++ 12-quiz-app/01-starting-project/src/components/Quiz.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
-import QUESTIONS from '../questions.js';
+import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
-//   Derived from   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-  const activeQuestionIndex = userAnswers.length
+  //   Derived from   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const activeQuestionIndex = userAnswers.length;
 
-  function handleAnswerQuiz() {
+  function handleAnswerQuiz(selectedAnswer) {
     // check if the answer is correct
     // if correct, add the quiz to the answeredQuiz
     // if wrong, do nothing
+
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
   }
 
-  return <p>Currently active Question</p>;
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleAnswerQuiz(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
