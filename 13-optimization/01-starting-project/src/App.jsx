bda84b6e2300fed8f3b56660400537d8e236e880
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-04 00:35:16 +0800

    feat: add millionjs for more optimization

--- 13-optimization/01-starting-project/src/App.jsx
+++ 13-optimization/01-starting-project/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
-import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
-  log('<App /> rendered');
+  log("<App /> rendered");
 
   const [chosenCount, setChosenCount] = useState(0);
 
   function handleSetCount(newCount) {
     setChosenCount(newCount);
-
   }
 
   return (
     <>
       <Header />
       <main>
-      <ConfigureCounter onSet={handleSetCount}/>
-        <Counter initialCount={chosenCount} />
+        <ConfigureCounter onSet={handleSetCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
