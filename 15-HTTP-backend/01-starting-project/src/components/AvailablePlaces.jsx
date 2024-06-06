7ef15b4661c30c762c29351eb630c234d728dc50
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-06 13:45:33 +0800

    feat: add handling of loading fetch requests

--- 15-HTTP-backend/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15-HTTP-backend/01-starting-project/src/components/AvailablePlaces.jsx
@@ -3,21 +3,26 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching ] = useState(false)
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
+      isLoading={isFetching}
+      loadingText={"Fetching place data..."}
       places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
