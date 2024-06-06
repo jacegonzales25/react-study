1367e97c7a40c17305b54764a624d47698c3ef02
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-06 13:59:45 +0800

    feat: add states for loading and error fetching data

--- 15-HTTP-backend/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15-HTTP-backend/01-starting-project/src/components/AvailablePlaces.jsx
@@ -3,20 +3,39 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching ] = useState(false)
+  // Error handling or typical handling of fetching, 3 states
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error} />;
+  }
 
   return (
     <Places
