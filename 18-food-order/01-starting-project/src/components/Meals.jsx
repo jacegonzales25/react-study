7b6d93966d714e1ddc56085f88b234bb504b9883
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-15 22:38:03 +0800

    feat: add MealItem component, formatting util

--- 18-food-order/01-starting-project/src/components/Meals.jsx
+++ 18-food-order/01-starting-project/src/components/Meals.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
+import MealItem from "./MealItem.jsx";
 
 export default function Meals() {
-    const [loadedMeals, setloadedMeals] = useState ([]);
+  const [loadedMeals, setloadedMeals] = useState([]);
 
-    useEffect(() => {
-        async function fetchMeals() {
-            try {
-              const response = await fetch("http://localhost:3000/meals");
-            } catch (error) {
-              console.log(error);
-            }
-            if (!response.ok) {
-                <p>No response</p>
-            }
-        
-            const meals = response.json()
-            setloadedMeals(meals);
-          }
-          fetchMeals();
-    }, [])
- 
+  useEffect(() => {
+    async function fetchMeals() {
+      try {
+        const response = await fetch("http://localhost:3000/meals");
+        const meals = await response.json();
+        setloadedMeals(meals);
+        if (!response.ok) {
+          <p>No response</p>;
+        }
+      } catch (error) {
+        throw new Error("Network response was not ok");
+      }
+    }
+    fetchMeals();
+  }, []);
 
-  return <ul id="meals">{
-loadedMeals.map((meal) => <li key={meal.id}>{meal.name}</li>)
-
-  }</ul>;
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
 }
