acdb37594c5ef9e8df06b487a6c4e383837844f6
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-18 14:33:14 +0800

    feat: add useHttp Custom hook, that handles Meals and Checkout state via fetch requests. fix: errors from infinite loop and multiple sendRequest

--- 18-food-order/01-starting-project/src/components/Meals.jsx
+++ 18-food-order/01-starting-project/src/components/Meals.jsx
@@ -1,24 +1,37 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp.js";
 import MealItem from "./MealItem.jsx";
 
-export default function Meals() {
-  const [loadedMeals, setloadedMeals] = useState([]);
+const requestConfig = {};
 
-  useEffect(() => {
-    async function fetchMeals() {
-      try {
-        const response = await fetch("http://localhost:3000/meals");
-        const meals = await response.json();
-        setloadedMeals(meals);
-        if (!response.ok) {
-          <p>No response</p>;
-        }
-      } catch (error) {
-        throw new Error("Network response was not ok");
-      }
-    }
-    fetchMeals();
-  }, []);
+export default function Meals() {
+  // const [loadedMeals, setloadedMeals] = useState([]);
+
+  // useEffect(() => {
+  //   async function fetchMeals() {
+  //     try {
+  //       const response = await fetch("http://localhost:3000/meals");
+  //       const meals = await response.json();
+  //       setloadedMeals(meals);
+  //       if (!response.ok) {
+  //         <p>No response</p>;
+  //       }
+  //     } catch (error) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //   }
+  //   fetchMeals();
+  // }, []);
+
+  const {
+    data: loadedMeals,
+    isLoading,
+    error
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+  if (isLoading) {
+    return <p>Fetching meals...</p>
+  }
 
   return (
     <ul id="meals">
---
+++ 18-food-order/01-starting-project/src/hooks/useHttp.js
@@ -0,0 +1,52 @@
+// Creating custom hooks since two HTTP fetch updates the UI
+
+import { useEffect, useState, useCallback } from "react";
+
+async function sendHttpRequest(url, config, initialData) {
+  const response = await fetch(url, config);
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      resData.message || "Something went wrong, failed to send request"
+    );
+  }
+
+  return resData;
+}
+
+export default function useHttp(url, config) {
+  // Update state via request status
+  const [data, setData] = useState(initialData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const sendRequest = useCallback(
+    async function sendRequest() {
+      setIsLoading(true);
+      try {
+        const resData = await sendHttpRequest(url, config);
+        setData(resData);
+      } catch (error) {
+        setError(error.message || "Something went wrong.");
+      }
+      setIsLoading(false);
+    },
+    [url, config]
+  );
+
+  useEffect(() => {
+    if (config && (config.method === 'GET' || !config.method) || !config) {    
+        sendRequest();
+    }
+    
+  }, [sendRequest, config]);
+
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequest,
+  };
+}
