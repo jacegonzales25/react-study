9abd4f0dcc4cf3b927fae32e0b622e8cf77ba87a
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-18 14:36:32 +0800

    feat: add custom Error component

--- 18-food-order/01-starting-project/src/components/Meals.jsx
+++ 18-food-order/01-starting-project/src/components/Meals.jsx
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from "react";
 import useHttp from "../hooks/useHttp.js";
 import MealItem from "./MealItem.jsx";
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 
@@ -30,7 +31,11 @@ export default function Meals() {
   } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
   if (isLoading) {
-    return <p>Fetching meals...</p>
+    return <p className="center">Fetching meals...</p>
+  }
+
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error}/>
   }
 
   return (
