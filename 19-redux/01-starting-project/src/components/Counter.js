29d4d26c2a186434712a658b6d5abefa49920b7a
jacegonzales25 <jacegonzales25@gmail.com>
2024-06-27 22:26:22 +0800

    feat: initialize redux project, add store: dispatch and state actions, add button

---
+++ 19-redux/01-starting-project/src/components/Counter.js
@@ -0,0 +1,36 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+
+  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const increase = () => {
+    dispatch({ type: 'increase', amount: 5})
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increase}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
