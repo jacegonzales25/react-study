551212eaeef480368b0d47a91ff277cf84aa5ad9
jacegonzales25 <jacegonzales25@gmail.com>
2024-07-02 10:38:51 +0800

    fix: add conditional render

--- 19-redux/01-starting-project/src/components/Header.js
+++ 19-redux/01-starting-project/src/components/Header.js
@@ -15,19 +15,21 @@ const Header = () => {
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">My Products</a>
-          </li>
-          <li>
-            <a href="/">My Sales</a>
-          </li>
-          <li>
-            {isLoggedIn && <button onClick={handleLogout}>Logout</button>}{" "}
-          </li>
-        </ul>
-      </nav>
+      {isLoggedIn && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
