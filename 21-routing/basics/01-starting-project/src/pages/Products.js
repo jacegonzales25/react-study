2527b815cb37e75cdc3dd3836a1ab462d9278458
jacegonzales25 <jacegonzales25@gmail.com>
2024-07-21 09:01:16 +0900

    feat: setup practice project

--- 21-routing/basics/01-starting-project/src/pages/Products.js
+++ 21-routing/basics/01-starting-project/src/pages/Products.js
@@ -1,11 +1,33 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "Red Scarf",
+    description: "A pretty red scarf.",
+  },
+  {
+    id: "p2",
+    title: "Blue T-Shirt",
+    description: "A pretty blue t-shirt.",
+  },
+  {
+    id: "p3",
+    title: "Green Trousers",
+    description: "A pair of lightly green trousers.",
+  },
+];
+
 function ProductsPage() {
   return (
     <>
       <h1>The Products Page</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
