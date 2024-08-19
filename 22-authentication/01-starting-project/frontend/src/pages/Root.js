1fa4df03a4b91d4ecdcd768172b390c09f784721
jacegonzales25 <jacegonzales25@gmail.com>
2024-08-19 14:30:05 +0800

    feat: add one hour automatic logout

--- 22-authentication/01-starting-project/frontend/src/pages/Root.js
+++ 22-authentication/01-starting-project/frontend/src/pages/Root.js
@@ -1,15 +1,28 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "POST" });
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
       <MainNavigation />
       <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
