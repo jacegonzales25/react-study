c9e219722e580120f3658fb018f9115efb8c9867
jacegonzales25 <jacegonzales25@gmail.com>
2024-08-19 14:08:01 +0800

    feat: add loader to route to get token and update the UI

--- 22-authentication/01-starting-project/frontend/src/App.js
+++ 22-authentication/01-starting-project/frontend/src/App.js
@@ -16,13 +16,16 @@ import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
-import { action as actionLogout} from "./pages/Logout";
+import { action as actionLogout } from "./pages/Logout";
+import { tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: "root",
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -69,9 +72,9 @@ const router = createBrowserRouter([
         action: newsletterAction,
       },
       {
-        path: 'logout',
-        action: actionLogout
-      }
+        path: "logout",
+        action: actionLogout,
+      },
     ],
   },
 ]);
--- 22-authentication/01-starting-project/frontend/src/util/auth.js
+++ 22-authentication/01-starting-project/frontend/src/util/auth.js
@@ -1,4 +1,12 @@
+
+
+
 export function getAuthToken() {
     const token = localStorage.getItem('token');
     return token;
+
+}
+
+export function tokenLoader() {
+    return getAuthToken();
 }
