183166851c315fb78a5642c747b18d50a0e75509
jacegonzales25 <jacegonzales25@gmail.com>
2024-08-26 09:58:03 +0800

    feat: finish challenge

--- 24-tanstack/starting-project/src/components/Events/EventDetails.jsx
+++ 24-tanstack/starting-project/src/components/Events/EventDetails.jsx
@@ -1,8 +1,38 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useParams } from "react-router-dom";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const { id } = useParams();
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["event", { eventId: id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+
+  const navigate = useNavigate();
+
+  const { mutate } = useMutation({
+    mutationFn: () => deleteEvent(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  const formattedDate = new Date(data?.date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+
+  function handleDelete() {
+    mutate({ id });
+  }
+
   return (
     <>
       <Outlet />
@@ -13,20 +43,29 @@ export default function EventDetails() {
       </Header>
       <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          {isLoading && <p>Fetching event...</p>}
+          {isError && (
+            <ErrorBlock
+              title={"Failed to fetch event"}
+              message={error.info?.message}
+            />
+          )}
+          <h1>{data?.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${data?.image}`} alt={data?.title} />
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{data?.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate + "@" + data?.time}
+              </time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{data?.description}</p>
           </div>
         </div>
       </article>
