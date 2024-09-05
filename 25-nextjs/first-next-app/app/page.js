7610042fa7823d9f0baf88e85c121193702ec61b
jacegonzales25 <jacegonzales25@gmail.com>
2024-09-05 11:14:44 +0800

    feat: finish nextJs basics

---
+++ 25-nextjs/first-next-app/app/page.js
@@ -0,0 +1,14 @@
+import Header from "@/components/header";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href={"/about"}>About Us</Link>
+      </p>
+    </main>
+  );
+}
