4d629b0cbec2f01bc4732a81241a3228b06e299b
jacegonzales25 <jacegonzales25@gmail.com>
2024-09-06 09:25:59 +0800

    feat: add loading via Suspense, remove loading.js

--- 25-nextjs/food-app/app/page.js
+++ 25-nextjs/food-app/app/page.js
@@ -6,21 +6,21 @@ import ImageSlideshow from "@/components/images/image-slideshow";
 export default function Home() {
   return (
     <>
-      <header className={classes.header}>
-        <div className={classes.slideshow}>
-          <ImageSlideshow />
+       <header className={classes.header}>
+      <div className={classes.slideshow}>
+        <ImageSlideshow />
+      </div>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Taste & share food from all over the world.</p>
         </div>
-        <div>
-          <div className={classes.hero}>
-            <h1>NextLevel Food for NextLevel Foodies</h1>
-            <p>Taste & share food from all over the world.</p>
-          </div>
-          <div className={classes.cta}>
-            <Link href="/community">Join the Community</Link>
-            <Link href="/meals">Explore Meals</Link>
-          </div>
+        <div className={classes.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
         </div>
-      </header>
+      </div>
+    </header>
       <main>
         <section className={classes.section}>
           <h2>How it works</h2>
