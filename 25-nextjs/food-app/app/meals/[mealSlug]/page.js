d011e13e1e023fad683ba6ca439f343f080abcbc
jacegonzales25 <jacegonzales25@gmail.com>
2024-09-06 20:47:43 +0800

    feat: add dynamic routes and loading and rendering it correctly

--- 25-nextjs/food-app/app/meals/[mealSlug]/page.js
+++ 25-nextjs/food-app/app/meals/[mealSlug]/page.js
@@ -1,8 +1,32 @@
-export default function MealDetails({params}) {
-    return (
-        <main>
-        <h1>{params.slug}</h1>
-        <p>This is {params.slug}</p>
-        </main>
-    );
+import Image from "next/image";
+import classes from "./page.module.css";
+
+export default function MealDetails({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, 
+  '<br />');
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
--- 25-nextjs/food-app/lib/meals.js
+++ 25-nextjs/food-app/lib/meals.js
@@ -1,8 +1,12 @@
-import sql from 'better-sqlite3'
+import sql from "better-sqlite3";
 
-const db = sql('meals.db')
+const db = sql("meals.db");
 
-export function getMeals() {
-   return db.prepare('SELECT * FROM meals').all();
+export  function getMeals() {
+  return db.prepare("SELECT * FROM meals").all();
 }
 
+// ? value as protection from SQLbase attacks
+export  function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug)
+}
