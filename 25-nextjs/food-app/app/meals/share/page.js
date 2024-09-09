e9bb344b9ba1cfc2ce38f7833b3a6f5fc443d403
jacegonzales25 <jacegonzales25@gmail.com>
2024-09-09 10:57:39 +0800

    feat: add server-side validations

--- 25-nextjs/food-app/app/meals/share/page.js
+++ 25-nextjs/food-app/app/meals/share/page.js
@@ -1,8 +1,9 @@
 // use client since server-side code has been migrated
-'use client';
+"use client";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
   return (
@@ -44,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />{" "}
           </p>
         </form>
       </main>
--- 25-nextjs/food-app/lib/actions.js
+++ 25-nextjs/food-app/lib/actions.js
@@ -6,6 +6,10 @@ import { saveMeal } from "./meals";
 export async function shareMeal(formData) {
   // Server action for submitting forms
 
+  function isInvalidText(text) {
+    return !text || text.trim() === "";
+  }
+
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -15,6 +19,18 @@ export async function shareMeal(formData) {
     creator_email: formData.get("email"),
   };
 
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instruction) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") || !meal.image || !meal.image.size === 0 
+  ) {
+    throw new Error('Invalid input');
+
+  }
+
   await saveMeal(meal);
-  redirect('/meals')
+  redirect("/meals");
 }
